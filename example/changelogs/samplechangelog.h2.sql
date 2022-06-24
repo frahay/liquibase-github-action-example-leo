bb618513be4f71960275a2f52e69b6335047be13
frahay <leorexij@gmail.com>
2022-06-24 12:35:45 -0400

    leo: Adding new changeset for example

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset your.name:1
+--changeset leo.rex:1
 --rollback DROP TABLE person;
 create table person (
     id int primary key,
@@ -10,7 +10,7 @@ create table person (
     city varchar(30)
 )
 
---changeset your.name:2
+--changeset leo.rex:2
 --rollback DROP TABLE company;
 create table company (
     id int primary key,
@@ -20,14 +20,24 @@ create table company (
     city varchar(30)
 )
 
---changeset other.dev:3
+--changeset cathy.c:3
 --rollback ALTER TABLE person DROP COLUMN country;
 alter table person add column country varchar(2)
 
---changeset other.dev:4
+--changeset cathy.c:4
 --rollback ALTER TABLE person DROP COLUMN state;
 alter table person add column state varchar(2)
 
---changeset other.dev:5
+--changeset cathy.c:5
 --rollback ALTER TABLE company DROP COLUMN country;
 alter table company add column country varchar(2)
+
+--changeset leo.rex:6
+--rollback DROP TABLE tesla;
+create table tesla (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
